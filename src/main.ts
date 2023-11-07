8a9c548500b064ac379fc16b294dd39d404621aa
Johan Abde <abdejohan@gmail.com>
2023-11-07 14:37:42 +0100

    Setup connection to unsplash api

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,23 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import './style.css';
+const apiKey = import.meta.env.VITE_UNSPLASH_API_KEY;
+const apiUrl = import.meta.env.VITE_UNSPLASH_API_URL;
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+/** HEADER:
+ * Accept-Version: v1
+ */
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+const main = async () => {
+  const data = await fetch(`${apiUrl}/photos/random`, {
+    headers: {
+      Authorization: 'Client-ID ' + apiKey,
+      'Content-Type': 'application/json',
+    },
+  });
+
+  const response = await data.json();
+  console.log(response);
+
+  console.log('fefe');
+};
+
+main();
