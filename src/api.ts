1608a756b9ea29e77e6ba60d48d2a62cbd2d11f8
Johan Abde <abdejohan@gmail.com>
2023-11-08 11:06:18 +0100

    Replace image container when images are fetched

--- src/api.ts
+++ src/api.ts
@@ -1,6 +1,10 @@
 const apiKey = import.meta.env.VITE_UNSPLASH_API_KEY;
 const apiUrl = import.meta.env.VITE_UNSPLASH_API_URL;
-
+type Image = {
+  urls: {
+    thumb: string;
+  };
+};
 /** HEADER:
  * Accept-Version: v1
  */
@@ -14,45 +18,48 @@ export const getPhotos = async () => {
   });
 
   const response = await data.json();
-  console.log(response);
-
-  console.log('found data');
   if (response) {
-    console.log('found data');
-    const imageContainer = document.querySelector(
-      '.image-container'
-    ) as HTMLElement;
-    response.forEach((image) => {
-      const img = document.createElement('img');
-      img.classList.add('image-container__item');
-      img.setAttribute('src', image.urls.thumb);
-      imageContainer.appendChild(img);
-    });
+    appendToView(response);
   }
 };
 
 export const eventListeners = () => {
-  console.log('im running');
-
   const element = document.querySelector('.search-container');
-  console.log(element);
-
   element?.addEventListener('submit', (e) => {
     e.preventDefault();
-    const form = e.target;
-       const userInputField = form.elements['searchInput'];
-       searchPhotos(userInputField.value)
+    const form = e.target as HTMLFormElement;
+    // @ts-expect-error expect error
+    const userInputField = form.elements['searchInput'];
+    searchPhotos(userInputField.value);
   });
 };
 
 export const searchPhotos = async (searchTerm: string) => {
-  const data = await fetch(`${apiUrl}/photos?page=1&per_page=9&query=${searchTerm}`, {
-    headers: {
-      Authorization: 'Client-ID ' + apiKey,
-      'Content-Type': 'application/json',
-    },
-  });
+  const data = await fetch(
+    `${apiUrl}/search/photos?page=1&per_page=9&query=${searchTerm}`,
+    {
+      headers: {
+        Authorization: 'Client-ID ' + apiKey,
+        'Content-Type': 'application/json',
+      },
+    }
+  );
 
   const response = await data.json();
-  console.log(response);
-}
+  if (response) {
+    appendToView(response.results);
+  }
+};
+
+const appendToView = (images: Image[]) => {
+  const imageContainer = document.querySelector(
+    '.image-container'
+  ) as HTMLElement;
+  imageContainer.innerHTML = '';
+  images.forEach((image: Image) => {
+    const img = document.createElement('img');
+    img.classList.add('image-container__item');
+    img.setAttribute('src', image.urls.thumb);
+    imageContainer.appendChild(img);
+  });
+};
