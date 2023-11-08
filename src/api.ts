7bd7e3e94256f03bb8d43c79db0afb6bdf5b8334
Felicia <felicia.lyden@gmail.com>
2023-11-08 11:58:37 +0100

    add state variables to history

--- src/api.ts
+++ src/api.ts
@@ -8,19 +8,19 @@ type Image = {
 
 type State = {
   pageNumber: number;
+  mode: string
 }
 /** HEADER:
  * Accept-Version: v1
  */
 
-const state = {
-  pageNumber: 1
+const state: State = {
+  pageNumber: 1,
+  mode: 'random'
 }
 
-state.pageNumber = window.history.state?.pageNumber || state.pageNumber
-
 export const getPhotos = async () => {
-  const data = await fetch(`${apiUrl}/photos?page=${state.pageNumber}&per_page=9`, {
+  const data = await fetch(`${apiUrl}/photos?page=${window.history.state.pageNumber}&per_page=9`, {
     headers: {
       Authorization: 'Client-ID ' + apiKey,
       'Content-Type': 'application/json',
@@ -29,12 +29,21 @@ export const getPhotos = async () => {
 
   const response = await data.json();
   if (response) {
+    window.history.pushState({ ...state, mode:'random'}, '')
     appendToView(response);
   }
 };
 
 const nextPage = () => {
-  window.history.pushState({pageNumber:state.pageNumber + 1}, '')
+  const { mode, searchTerm } = window.history.state
+  state.pageNumber = state.pageNumber + 1
+  window.history.pushState({ ...state, pageNumber:state.pageNumber}, '')
+  if(mode === 'random'){
+    getPhotos()
+  }
+  if(mode === 'search'){
+    searchPhotos(searchTerm)
+  }
 }
 
 export const eventListeners = () => {
@@ -44,6 +53,7 @@ export const eventListeners = () => {
     const form = e.target as HTMLFormElement;
     // @ts-expect-error expect error
     const userInputField = form.elements['searchInput'];
+    window.history.pushState({ ...state, searchTerm: userInputField.value}, '')
     searchPhotos(userInputField.value);
   });
   const buttonNext = document.querySelector("#next");
@@ -51,9 +61,8 @@ export const eventListeners = () => {
 };
 
 export const searchPhotos = async (searchTerm: string) => {
-
   const data = await fetch(
-    `${apiUrl}/search/photos?page=${state.pageNumber}&per_page=9&query=${searchTerm}`,
+    `${apiUrl}/search/photos?page=${window.history.state.pageNumber}&per_page=9&query=${searchTerm}`,
     {
       headers: {
         Authorization: 'Client-ID ' + apiKey,
@@ -64,6 +73,7 @@ export const searchPhotos = async (searchTerm: string) => {
 
   const response = await data.json();
   if (response) {
+    window.history.pushState({ ...state, mode:'search'}, '')
     appendToView(response.results);
   }
 };
